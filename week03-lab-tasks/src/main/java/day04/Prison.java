afa4ba7cb4b3bb603dd684945a970732919a3380
peterpetrekanics <peter.petrekanics@liferay.com>
2021-10-28 16:08:59 +0200

    Sultan exercise completed

---
+++ week03-lab-tasks/src/main/java/day04/Prison.java
@@ -0,0 +1,21 @@
+package day04;
+
+import java.util.Arrays;
+
+public class Prison {
+	private boolean[] cells = new boolean[100];
+
+	public boolean[] getCells() {
+		return cells;
+	}
+
+	public void openFreeCells() {
+		for (int i = 0; i < cells.length; i++) {
+			for (int j = i; j < cells.length; j++) {
+				if ((j + 1) % (i + 1)==0) {
+					cells[j] = !cells[j];
+				}
+			}
+		}
+	}
+}
---
+++ week03-lab-tasks/src/main/java/day04/Sultan.java
@@ -0,0 +1,15 @@
+package day04;
+
+import java.util.Arrays;
+
+public class Sultan {
+	public static void main(String[] args) {
+		Prison prison = new Prison();
+		prison.openFreeCells();
+		for (int i = 0; i < prison.getCells().length; i++) {
+			if (prison.getCells()[i]) {
+				System.out.println(i + 1);
+			}
+		}
+	}
+}
